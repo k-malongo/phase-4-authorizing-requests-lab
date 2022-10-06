c87425c968b454dd021f9024d77c579e0e7ea7c4
Kelvin <vokemalosh32@gmail.com>
2022-10-06 14:51:36 +0300

    commit tree 090a677790096394449e7c12e0ada31525a97012

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,9 @@ class UsersController < ApplicationController
     user = User.find(session[:user_id])
     render json: user
   end
+  def index 
+    user = User.all
+    render json: user 
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :members_only_articles, only: [:index, :show]
 
   get "/me", to: "users#show"
-
+  get "/users", to: "users#index"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  get "/members_only_articles", to: "members_only_articles#index"
+  get "/members_only_articles/:id", to: "members_only_articles#show"
 end
